252b2260cc61f702e80f1a21278d2764052af876
Abderrahmane <mohamed.mohamed@stud.th-owl.de>
2025-05-27 22:34:09 +0200

    Frontend neue Dateien hinzugefügt, mit React.js

---
+++ fledermaus-tracking-frontend/src/App.js
@@ -0,0 +1,10 @@
+
+import AppPage from './pages/App';
+import './styles/main.css';
+import './styles/variables.css';
+
+function App() {
+  return <AppPage />;
+}
+
+export default App;
---
+++ fledermaus-tracking-frontend/src/pages/App.jsx
@@ -0,0 +1,11 @@
+
+const AppPage = () => {
+
+  return (
+    <div className="container">
+      
+    </div>
+  );
+};
+
+export default AppPage;
